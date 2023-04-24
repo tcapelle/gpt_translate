e5b79e9bc77a291dda39dd028fa4771b10d84c7b
Thomas Capelle <tcapelle@pm.me>
2023-04-24 23:19:12 +0200

    split at blank lines also

--- gpt_translate/utils.py
+++ gpt_translate/utils.py
@@ -7,16 +7,18 @@ def split_markdown_file(file: Path, min_lines: int = 50):
         lines = f.readlines()
 
     header_pattern = re.compile(r"^#{1,6} .*$")
+    double_or_empty_line_pattern = re.compile(r"^\s*$")
 
     chunks = []
     chunk = []
     line_count = 0
 
     for line in lines:
-        if header_pattern.match(line) and line_count >= min_lines:
-            chunks.append("".join(chunk))
-            chunk = []
-            line_count = 0
+        if line_count >= min_lines:
+            if header_pattern.match(line) or double_or_empty_line_pattern.match(line):
+                chunks.append("".join(chunk))
+                chunk = []
+                line_count = 0
 
         chunk.append(line)
         line_count += 1
@@ -39,5 +41,7 @@ def count_file_lines(file: Path):
 
 if __name__ == "__main__":
     chunks = split_markdown_file("docs/intro.md")
-    print(len(chunks))
+
     print(chunks[0])
+    print(len(chunks[0]))
+    print(len(chunks))
