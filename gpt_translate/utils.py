dbe02ff0f1b7576249e4b43dd6af873fb858d8f3
Thomas Capelle <tcapelle@pm.me>
2023-12-19 16:07:04 +0100

    check emptyness of file

--- gpt_translate/utils.py
+++ gpt_translate/utils.py
@@ -1,13 +1,10 @@
-import re
 import time
-
-import tiktoken
-
-from fastcore.script import call_parse, Param, store_true
-from fastcore.xtras import globtastic
-
 from pathlib import Path
 
+from fastcore.xtras import globtastic
+import tiktoken
+
+MODEL = "gpt-4"
 
 def measure_execution_time(func):
     def wrapper(*args, **kwargs):
@@ -19,11 +16,10 @@ def measure_execution_time(func):
         return result
     return wrapper
 
-def check_file_non_empty(file: Path):
-    with open(file, "r") as f:
-        lines = f.readlines()
-
-    return bool(lines)
+def file_is_empty(file: Path):
+    with open(file, 'r', encoding='utf-8') as file:
+        content = file.read()
+        return not content.strip()
 
 
 def get_md_files(path: Path | str, files_glob: str = "*.md", file_re: str = None):
@@ -34,39 +30,7 @@ def get_md_files(path: Path | str, files_glob: str = "*.md", file_re: str = None
     return [Path(f) for f in files.sorted()]
 
 
-@call_parse
-def delete_empty_files(
-    path: Param("Path to delete empty files from", str),
-    verbose: Param("Verbose", store_true) = False,
-):
-    for f in get_md_files(path):
-        if not check_file_non_empty(f):
-            if verbose:
-                print(f"Deleting {f}")
-            f.unlink()
-
-
-def remove_newline(path, verbose=False, pct=0.5):
-    for file in get_md_files(path):
-        if check_file_non_empty(file):
-            _maybe_remove_odd_lines(file, verbose=verbose, pct=pct)
-
-
-def _maybe_remove_odd_lines(path, verbose=False, pct=0.5):
-    with open(path, "r") as f:
-        lines = f.readlines()
-    if len([line for line in lines if line == "\n"]) / len(lines) > pct:
-        if verbose:
-            print(f"Removing odd lines from {path}")
-        with open(path, "w") as f:
-            for i in range(len(lines)):
-                if i % 2 == 0:
-                    f.write(lines[i])
-    else:
-        if verbose:
-            print(f"Skipping: {path}")
-
-def count_tokens(chunk, model="gpt-4"):
+def count_tokens(chunk, model=MODEL):
     "Count the number of tokens in a chunk"
     enc = tiktoken.encoding_for_model(model)
     return len(enc.encode(chunk))
